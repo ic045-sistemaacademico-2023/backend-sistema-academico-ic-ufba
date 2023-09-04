5d5fa14d7586ac28b49cdd1e239ae759e95d9622
Vitor de Jesus <92599145+VitaminaNescau@users.noreply.github.com>
2023-09-04 17:43:56 -0300

    [FEAT] ultima versão do banco

--- src/main/java/com/ic045/sistemaacademico/utils/migrations/Database.sql
+++ src/main/java/com/ic045/sistemaacademico/utils/migrations/Database.sql
@@ -4,116 +4,123 @@ CREATE DATABASE IF NOT EXISTS SistemaAcademico;
 USE SistemaAcademico;
 
 
-CREATE TABLE
-  `Aluno` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `id_curso` INT NOT NULL,
-    `nome` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) UNIQUE NOT NULL,
-    `senha` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-  );
+CREATE TABLE `aluno` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`id_curso` int NOT NULL,
+	`id_turma` INT NOT NULL,
+	`id_usuario` INT NOT NULL,
+	`nome` varchar(255) NOT NULL,
+	`cr` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
-CREATE TABLE
-  `CoordenadorDeCurso` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `nome` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) UNIQUE NOT NULL,
-    `senha` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-  );
+CREATE TABLE `coordenadordecurso` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`id_usuario` INT NOT NULL,
+	`nome` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL UNIQUE,
+	`senha` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
-  CREATE TABLE
-  `Curso` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `id_coordenador` INT NOT NULL,
-    `nome` VARCHAR(255) NOT NULL,
-    `semestre` INT NOT NULL,
-    `turno` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id_coordenador) REFERENCES CoordenadorDeCurso (id)
-  );
+CREATE TABLE `curso` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`id_coordenador` int NOT NULL,
+	`nome` varchar(255) NOT NULL,
+	`semestre` int NOT NULL,
+	`turno` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
-CREATE TABLE
-  `Materia` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-        `id_curso` INT NOT NULL,
-    `nome` VARCHAR(255) NOT NULL,
-    `codigo` VARCHAR(255) UNIQUE NOT NULL,
-    `ementa` VARCHAR(255) NOT NULL,
-    `pre_requisitos` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id_curso) REFERENCES Curso (id)
-  );
+CREATE TABLE `disciplina` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`id_curso` int NOT NULL,
+	`nome` varchar(255) NOT NULL,
+	`codigo` varchar(255) NOT NULL UNIQUE,
+	`ementa` varchar(255) NOT NULL,
+	`pre_requisitos` varchar(255) NOT NULL,
+	`nivel` VARCHAR(255),
+	`area` varchar(255),
+	`observacao` VARCHAR(255),
+	`ch` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
-CREATE TABLE
-  `Administrador` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `nome` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) UNIQUE NOT NULL,
-    `senha` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-  );
+CREATE TABLE `administrador` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`id_usuario` INT NOT NULL,
+	`nome` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL UNIQUE,
+	`senha` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
-  CREATE TABLE
-  `Nota` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `id_aluno` INT NOT NULL,
-    `id_materia` INT NOT NULL,
-    `nota` DECIMAL(10, 2) NOT NULL,
-    `semestre` INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id_aluno) REFERENCES Aluno (id),
-    FOREIGN KEY (id_materia) REFERENCES Materia (id)
-  );
+CREATE TABLE `nota` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`id_aluno` int NOT NULL,
+	`id_turma` int NOT NULL,
+	`nota` DECIMAL(10,2) NOT NULL,
+	`semestre` int NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
-CREATE TABLE
-  `Professor` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `nome` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) UNIQUE NOT NULL,
-    `senha` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-  );
+CREATE TABLE `professor` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`id_usuario` INT NOT NULL,
+	`nome` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL UNIQUE,
+	`senha` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
-CREATE TABLE
-  `Turma` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `id_materia` INT NOT NULL,
-    `id_professor` INT NOT NULL,
-    `dias` VARCHAR(255) NOT NULL,
-    `horario` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (id_materia) REFERENCES Materia (id),
-    FOREIGN KEY (id_professor) REFERENCES Professor (id)
-  );
+CREATE TABLE `turma` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`id_disciplina` int NOT NULL,
+	`id_professor` int NOT NULL,
+	`dias` varchar(255) NOT NULL,
+	`horario` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `usuario` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`cpf` VARCHAR(255) NOT NULL UNIQUE,
+	`senha` VARCHAR(255) NOT NULL,
+	`email` VARCHAR(255) NOT NULL UNIQUE,
+	`role` VARCHAR(255) NOT NULL,
+	`status` VARCHAR(255) NOT NULL,
+	`nome` VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `aluno` ADD CONSTRAINT `aluno_fk0` FOREIGN KEY (`id_curso`) REFERENCES `curso`(`id`);
+
+ALTER TABLE `aluno` ADD CONSTRAINT `aluno_fk1` FOREIGN KEY (`id_turma`) REFERENCES `turma`(`id`);
+
+ALTER TABLE `aluno` ADD CONSTRAINT `aluno_fk2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario`(`id`);
+
+ALTER TABLE `coordenadordecurso` ADD CONSTRAINT `coordenadordecurso_fk0` FOREIGN KEY (`id_usuario`) REFERENCES `usuario`(`id`);
+
+ALTER TABLE `curso` ADD CONSTRAINT `curso_fk0` FOREIGN KEY (`id_coordenador`) REFERENCES `coordenadordecurso`(`id`);
+
+ALTER TABLE `disciplina` ADD CONSTRAINT `disciplina_fk0` FOREIGN KEY (`id_curso`) REFERENCES `curso`(`id`);
+
+ALTER TABLE `administrador` ADD CONSTRAINT `administrador_fk0` FOREIGN KEY (`id_usuario`) REFERENCES `usuario`(`id`);
+
+ALTER TABLE `nota` ADD CONSTRAINT `nota_fk0` FOREIGN KEY (`id_aluno`) REFERENCES `aluno`(`id`);
+
+ALTER TABLE `nota` ADD CONSTRAINT `nota_fk1` FOREIGN KEY (`id_turma`) REFERENCES `turma`(`id`);
+
+ALTER TABLE `professor` ADD CONSTRAINT `professor_fk0` FOREIGN KEY (`id_usuario`) REFERENCES `usuario`(`id`);
+
+ALTER TABLE `turma` ADD CONSTRAINT `turma_fk0` FOREIGN KEY (`id_disciplina`) REFERENCES `disciplina`(`id`);
+
+ALTER TABLE `turma` ADD CONSTRAINT `turma_fk1` FOREIGN KEY (`id_professor`) REFERENCES `professor`(`id`);
 
 
-ALTER TABLE
-  `Nota`
-ADD
-  FOREIGN KEY (`id_materia`) REFERENCES `Materia` (`id`);
 
 
-ALTER TABLE
-  `Aluno`
-ADD
-  FOREIGN KEY (`id_curso`) REFERENCES `Curso` (`id`);
 
 
-ALTER TABLE
-  `Materia`
-ADD
-  FOREIGN KEY (`id_curso`) REFERENCES `Curso` (`id`);
 
 
-ALTER TABLE
-  `Turma`
-ADD
-  FOREIGN KEY (`id_materia`) REFERENCES `Materia` (`id`);
-
-
-ALTER TABLE
-  `Turma`
-ADD
-  FOREIGN KEY (`id_professor`) REFERENCES `Professor` (`id`);
