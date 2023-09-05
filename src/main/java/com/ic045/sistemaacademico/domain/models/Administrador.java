bce2c29a72992d5b65183fa3570490aca5403f3c
vitaminanescau <vitorjss82@gmail.com>
2023-09-05 18:14:05 -0300

    [WIP] adicionado Getter Setter via lombok

--- src/main/java/com/ic045/sistemaacademico/domain/models/Administrador.java
+++ src/main/java/com/ic045/sistemaacademico/domain/models/Administrador.java
@@ -1,7 +1,15 @@
 package com.ic045.sistemaacademico.domain.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "administrador")
 @Table(name = "administrador")
 public class Administrador {
--- src/main/java/com/ic045/sistemaacademico/domain/models/Professor.java
+++ src/main/java/com/ic045/sistemaacademico/domain/models/Professor.java
@@ -7,7 +7,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "professor")
 public class Professor {
