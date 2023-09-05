2b94b772dc7c2d30567f6e796ff2193c44262633
vitaminanescau <vitorjss82@gmail.com>
2023-09-05 00:56:28 -0300

    [WIP(FIX)] Alterado atributo String id para o tipo Long, padronização dos id.

--- src/main/java/com/ic045/sistemaacademico/repositories/UsuarioRepository.java
+++ src/main/java/com/ic045/sistemaacademico/repositories/UsuarioRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 import com.ic045.sistemaacademico.domain.models.Usuario;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
