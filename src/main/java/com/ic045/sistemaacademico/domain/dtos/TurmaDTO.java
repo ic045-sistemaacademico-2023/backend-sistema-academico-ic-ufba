d5ef9e63e8ba6ae4a01538a38180d226cae6939b
Elis Alcantara <emarcelatfs@gmail.com>
2023-09-13 22:29:40 -0300

    [WIP] Ajustando DTO e builder

--- src/main/java/com/ic045/sistemaacademico/domain/dtos/TurmaDTO.java
+++ src/main/java/com/ic045/sistemaacademico/domain/dtos/TurmaDTO.java
@@ -12,11 +12,19 @@ import lombok.ToString;
 @Builder
 @ToString
 public class TurmaDTO {
-
-    private int idDisciplina;
-    private int idProfessor;
+    private Long id;
     private String dias;
     private String horario;
+    private String local;
+    private String semestre;
+    private String disciplinaNome;
+
+    public TurmaDTO(Long id, String disciplinaNome, String dias, String horario) {
+        this.id = id;
+        this.disciplinaNome = disciplinaNome;
+        this.dias = dias;
+        this.horario = horario;
+    }
 
     @ToString.Exclude
     private DisciplinaDTO disciplina;
