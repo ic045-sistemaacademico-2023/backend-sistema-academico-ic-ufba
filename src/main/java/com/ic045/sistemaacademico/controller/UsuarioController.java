d2b6bf1db8c82faccbe1eed3c19ae47fe5dc9309
Karen Botelho <12010728+NyKaren@users.noreply.github.com>
2023-09-17 15:30:30 -0300

    Created the endpoint to get all the users.

--- src/main/java/com/ic045/sistemaacademico/controller/UsuarioController.java
+++ src/main/java/com/ic045/sistemaacademico/controller/UsuarioController.java
@@ -1,8 +1,13 @@
 package com.ic045.sistemaacademico.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ic045.sistemaacademico.domain.models.Usuario;
 import com.ic045.sistemaacademico.services.UsuarioService;
@@ -10,13 +15,20 @@ import com.ic045.sistemaacademico.services.UsuarioService;
 @RestController
 @RequestMapping("/user")
 public class UsuarioController {
-    @Autowired
-    private UsuarioService service;
+	@Autowired
+	private UsuarioService service;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Usuario> findById(@PathVariable Long id) {
-        Usuario usuario = service.findById(id);
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> findById(@PathVariable Long id) {
+		Usuario usuario = service.findById(id);
 
-        return usuario != null ? ResponseEntity.ok(usuario): ResponseEntity.notFound().build();
-    }
+		return usuario != null ? ResponseEntity.ok(usuario) : ResponseEntity.notFound().build();
+	}
+
+	@GetMapping("/all")
+	public ResponseEntity<List<Usuario>> findAll() {
+		List<Usuario> usuarios = service.findAll();
+
+		return usuarios != null ? ResponseEntity.ok(usuarios) : ResponseEntity.notFound().build();
+	}
 }
