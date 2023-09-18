f13bad9e4692d852a82647bbdef1dc20a633cf02
vitaminanescau <vitorjss82@gmail.com>
2023-09-18 19:30:13 -0300

    [FEAT] Adicionado inserção de dados

--- src/main/java/com/ic045/sistemaacademico/services/CursoService.java
+++ src/main/java/com/ic045/sistemaacademico/services/CursoService.java
@@ -3,6 +3,8 @@ package com.ic045.sistemaacademico.services;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
 import com.ic045.sistemaacademico.domain.models.Curso;
@@ -13,6 +15,16 @@ public class CursoService {
     @Autowired
     private CursoRepository repository;
 
+    public Boolean InsertCursoData(Curso curso){
+        if (repository.exists(Example.of(curso))){return false;}
+        try {
+            repository.save(curso);
+            return true;
+        }catch (IllegalArgumentException e){
+            return   false;
+        }
+
+    }
     public Curso findById(Long id) {
         try {
             return repository.findById(id).get() ;
@@ -20,4 +32,5 @@ public class CursoService {
             return null;
         }
     }
+
 }
