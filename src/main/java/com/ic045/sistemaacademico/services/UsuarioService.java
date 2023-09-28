91aab00bf6bda02fdf73418e44d3da46209c3d98
vitaminanescau <vitorjss82@gmail.com>
2023-09-28 19:20:35 -0300

    [WIP] removido imports

--- src/main/java/com/ic045/sistemaacademico/services/UsuarioService.java
+++ src/main/java/com/ic045/sistemaacademico/services/UsuarioService.java
@@ -2,16 +2,11 @@ package com.ic045.sistemaacademico.services;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 
 import com.ic045.sistemaacademico.domain.models.Role;
-
-import com.ic045.sistemaacademico.exception.custom.NotFoundException;
-import com.ic045.sistemaacademico.utils.constants.ErrorMessages;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.ic045.sistemaacademico.domain.models.Usuario;
 import com.ic045.sistemaacademico.repositories.UsuarioRepository;
 
@@ -44,6 +39,6 @@ public class UsuarioService {
 	public Usuario insertUsuario(Usuario request) {
 		request.setStatus(Role.Status.EMAIL_CHECK);
 
-        return repository.save(request);
+		return repository.save(request);
     }
 }
