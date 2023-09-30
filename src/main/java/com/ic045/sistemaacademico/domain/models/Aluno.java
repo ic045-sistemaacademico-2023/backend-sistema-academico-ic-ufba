60dc133cde01cc78300202542fcb8dfb8cf8919b
vitaminanescau <vitorjss82@gmail.com>
2023-09-30 19:29:17 -0300

    [WIP] Comentado para verificação de erro

--- src/main/java/com/ic045/sistemaacademico/domain/models/Aluno.java
+++ src/main/java/com/ic045/sistemaacademico/domain/models/Aluno.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 @Getter
 @Setter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @RequiredArgsConstructor
@@ -21,12 +22,12 @@ public class Aluno {
     @JoinColumn(name = "id_usuario", referencedColumnName = "id",unique = true)
     private Usuario usuario;
 
-    @ManyToMany
-    @JoinTable(
-        name = "aluno_turma",
-        joinColumns = @JoinColumn(name = "id_aluno"),
-        inverseJoinColumns = @JoinColumn(name = "id_turma"))
-    private Set<Turma> turmas;
+//    @ManyToMany
+//    @JoinTable(
+//        name = "aluno_turma",
+//        joinColumns = @JoinColumn(name = "id_aluno"),
+//        inverseJoinColumns = @JoinColumn(name = "id_turma"))
+//    private Set<Turma> turmas;
     @NonNull
     @ManyToOne
     @JoinColumn(name = "id_curso", referencedColumnName = "id")
