0ff62ea12a4a7a7acf0349fff4f95febf4d0241b
vitaminanescau <vitorjss82@gmail.com>
2023-09-30 00:12:59 -0300

    [FEAT] body de post

--- src/main/java/com/ic045/sistemaacademico/controller/vos/request/InsertTurmaRequest.java
+++ src/main/java/com/ic045/sistemaacademico/controller/vos/request/InsertTurmaRequest.java
@@ -1,4 +1,11 @@
 package com.ic045.sistemaacademico.controller.vos.request;
 
-public record InsertTurmaRequest() {
+public record InsertTurmaRequest(
+        Long disciplina,
+        Long professor,
+        String dias,
+        String horario,
+        String local,
+        String semestre
+) {
 }
