e6d5ce678c7c42daa6341642721af5723b5fa1f7
vitaminanescau <vitorjss82@gmail.com>
2023-09-30 17:46:04 -0300

    [FEAT] inserção de turmas

--- src/main/java/com/ic045/sistemaacademico/controller/TurmaController.java
+++ src/main/java/com/ic045/sistemaacademico/controller/TurmaController.java
@@ -1,8 +1,12 @@
 package com.ic045.sistemaacademico.controller;
 
+import com.ic045.sistemaacademico.controller.vos.request.InsertTurmaRequest;
+import com.ic045.sistemaacademico.domain.models.Disciplina;
+import com.ic045.sistemaacademico.domain.models.Professor;
 import com.ic045.sistemaacademico.domain.models.Turma;
 import com.ic045.sistemaacademico.services.TurmaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,7 +17,8 @@ import java.util.List;
 public class TurmaController {
     @Autowired
     private TurmaService service;
-
+    private Disciplina disciplina;
+    private Professor professor;
     @GetMapping("/{id}")
     public ResponseEntity<Turma> findById(@PathVariable Long id) {
         Turma turma = service.findById(id);
@@ -29,5 +34,17 @@ public class TurmaController {
         List<Turma> turmas = service.findForSemestreData(period);
 
         return !turmas.isEmpty() ? ResponseEntity.ok(turmas):ResponseEntity.notFound().build();
+    }
+    @PostMapping("/")
+    public ResponseEntity<Boolean> InsertTurma(@RequestBody InsertTurmaRequest InsertTurma){
+        disciplina.setId(InsertTurma.disciplina());
+        Turma turma;
+        try {
+            professor.setId(InsertTurma.professor());
+             turma = new Turma(disciplina,professor,InsertTurma.dias(),InsertTurma.horario(), InsertTurma.local(), InsertTurma.semestre());
+        }catch (NullPointerException e){
+             turma = new Turma(disciplina,null,InsertTurma.dias(),InsertTurma.horario(), InsertTurma.local(), InsertTurma.semestre());
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.InsertTurmaData(turma));
     }
 }
