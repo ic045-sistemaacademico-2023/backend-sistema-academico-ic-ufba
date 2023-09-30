d8b86a6bce0e06b5f625938b23773808a88c3d5c
vitaminanescau <vitorjss82@gmail.com>
2023-09-30 00:11:58 -0300

    [FEAT] Inserção de turma

--- src/main/java/com/ic045/sistemaacademico/services/TurmaService.java
+++ src/main/java/com/ic045/sistemaacademico/services/TurmaService.java
@@ -2,9 +2,12 @@ package com.ic045.sistemaacademico.services;
 
 import java.util.List;
 
+import com.ic045.sistemaacademico.exception.custom.NotCreatedException;
 import com.ic045.sistemaacademico.exception.custom.NotFoundException;
 import com.ic045.sistemaacademico.utils.constants.ErrorMessages;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import com.ic045.sistemaacademico.domain.models.Turma;
@@ -28,5 +31,14 @@ public class TurmaService {
 
     public List<Turma> findAllByAluno(Long idAluno) {
         return repository.findAllByAluno(idAluno);
+    }
+
+    public Boolean InsertTurmaData(Turma turma) {
+        try {
+           if (repository.exists(Example.of(turma)))throw new NotCreatedException(ErrorMessages.NOT_CREATED.getMessage());
+           repository.save(turma);
+            return true;
+        }catch (IllegalArgumentException e){throw  new NotCreatedException(ErrorMessages.DATA_NULL.getMessage());}
+        catch (OptimisticLockingFailureException e){throw new NotCreatedException(ErrorMessages.NOT_CREATED.getMessage());}
     }
 }
