518a358779a11dc7787261d4d1c691c68103f470
vitaminanescau <vitorjss82@gmail.com>
2023-10-01 13:43:50 -0300

    [WIP] Adicionado metodo para buscar de turmas pelo aluno

--- src/main/java/com/ic045/sistemaacademico/repositories/TurmaRepository.java
+++ src/main/java/com/ic045/sistemaacademico/repositories/TurmaRepository.java
@@ -1,8 +1,6 @@
 package com.ic045.sistemaacademico.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ic045.sistemaacademico.domain.models.Turma;
