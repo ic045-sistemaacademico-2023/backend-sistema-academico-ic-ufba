3c8b9281803cac83d9c4c048efebb3823c13b6cd
vitaminanescau <vitorjss82@gmail.com>
2023-10-01 06:59:42 -0300

    [WIP] Adição de construtor

--- src/main/java/com/ic045/sistemaacademico/domain/models/Turma.java
+++ src/main/java/com/ic045/sistemaacademico/domain/models/Turma.java
@@ -38,4 +38,12 @@ public class Turma {
     private String local;
     @NonNull
     private String semestre;
+
+    public Turma(@NonNull Disciplina disciplina, @NonNull String dias, @NonNull String horario, @NonNull String local, @NonNull String semestre) {
+        this.disciplina = disciplina;
+        this.dias = dias;
+        this.horario = horario;
+        this.local = local;
+        this.semestre = semestre;
+    }
 }
