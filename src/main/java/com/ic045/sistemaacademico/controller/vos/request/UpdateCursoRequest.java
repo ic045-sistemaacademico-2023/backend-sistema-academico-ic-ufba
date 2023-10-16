07a68c34cef5317a1c6e4111784b0d70ca8aa582
Karen Botelho <12010728+NyKaren@users.noreply.github.com>
2023-10-16 02:29:49 -0300

    Curso CRUD

--- src/main/java/com/ic045/sistemaacademico/controller/vos/request/UpdateCursoRequest.java
+++ src/main/java/com/ic045/sistemaacademico/controller/vos/request/UpdateCursoRequest.java
@@ -2,5 +2,5 @@ package com.ic045.sistemaacademico.controller.vos.request;
 
 import com.ic045.sistemaacademico.domain.models.Role;
 
-public record UpdateCursoRequest(String nome, String semestre, Role.Shift turno, Long coordenador) {
+public record UpdateCursoRequest(String nome, int semestre, Role.Shift turno, Long coordenador) {
 }
