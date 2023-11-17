0d42e564cc176a4304eb2e262d22854e2ff6317d
IsaqueCopque <71469931+IsaqueCopque@users.noreply.github.com>
2023-11-17 10:27:02 -0300

    fix post endpoint

--- src/main/java/com/ic045/sistemaacademico/controller/NotaController.java
+++ src/main/java/com/ic045/sistemaacademico/controller/NotaController.java
@@ -1,13 +1,5 @@
 package com.ic045.sistemaacademico.controller;
 
-import com.ic045.sistemaacademico.domain.models.Nota;
-import com.ic045.sistemaacademico.domain.models.Usuario;
-import com.ic045.sistemaacademico.domain.models.Aluno;
-import com.ic045.sistemaacademico.domain.models.Turma;
-import com.ic045.sistemaacademico.services.NotaService;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ic045.sistemaacademico.controller.vos.request.InsertNotaRequest;
-import com.ic045.sistemaacademico.controller.vos.request.InsertTurmaRequest;
-import com.ic045.sistemaacademico.controller.vos.request.UpdateNotaRequest;
+import com.ic045.sistemaacademico.domain.models.Nota;
+import com.ic045.sistemaacademico.services.NotaService;
+
 
 @RestController
 @RequestMapping("/nota")
@@ -38,6 +32,8 @@ public class NotaController {
 
     @PostMapping("/")
     public ResponseEntity<Nota> insertNota(@RequestBody InsertNotaRequest insertNotaRequest){
+    	if(insertNotaRequest.nota() == null)
+    		return ResponseEntity.badRequest().build();
         return ResponseEntity.status(HttpStatus.CREATED).body(service.insertNotaData(insertNotaRequest));
     }
 
@@ -49,7 +45,7 @@ public class NotaController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Nota> updateNota(@PathVariable Long id, @RequestBody UpdateNotaRequest request) {
+    public ResponseEntity<Nota> updateNota(@PathVariable Long id, @RequestBody InsertNotaRequest request) {
         Nota nota = service.updateNota(id, request);
 
         return ResponseEntity.status(HttpStatus.OK).body(nota);
