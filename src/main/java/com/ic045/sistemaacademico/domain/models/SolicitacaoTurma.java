42ced95ba51add3410b83b0e42c38c087e984f22
Elis Alcantara <emarcelatfs@gmail.com>
2023-11-25 15:44:25 -0300

    Table

--- src/main/java/com/ic045/sistemaacademico/domain/models/SolicitacaoTurma.java
+++ src/main/java/com/ic045/sistemaacademico/domain/models/SolicitacaoTurma.java
@@ -5,12 +5,14 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "solicitacao_turma")
 public class SolicitacaoTurma {
     @Id
     private Long id;
