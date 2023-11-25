9309bc21fd31d0dd1a8ff682220585ccaa2bdfd2
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2023-11-25 17:44:00 -0300

    fix import

--- src/main/java/com/ic045/sistemaacademico/controller/SolicitacaoMatriculaController.java
+++ src/main/java/com/ic045/sistemaacademico/controller/SolicitacaoMatriculaController.java
@@ -5,17 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.ic045.sistemaacademico.domain.models.SolicitacaoMatricula;
 import com.ic045.sistemaacademico.services.SolicitacaoMatriculaService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/solicitacao-matricula")
